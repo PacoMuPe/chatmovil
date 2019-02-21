d5491e8980bbf9c1d2217160de4342722025541b
Paco Muñoz <pacomunozperales@gmail.com>
2019-02-21 15:02:19 +0100

    primer diseño

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,5 +1,18 @@
 <template>
-  <router-link to="/about">About</router-link>
+  <div class="list fixed pin">
+    <router-link to="/">Home</router-link>
+    <div class="bg-grey-dark">
+      <h3 class="h3">Chats activos</h3>
+    </div>
+    <div class="bg-white">
+      <div class="buscador">
+        <div class="buscador-interior">
+          <i class="fas fa-search"></i>
+          <input type="text">
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,3 +22,25 @@ export default {
   }
 }
 </script>
+
+<style lang="postcss" scoped>
+
+  .h3 {
+    @apply py-4 text-center text-white font-normal;
+  }
+
+  .buscador {
+    @apply py-2
+  }
+
+  .buscador-interior {
+    @apply py-4
+  }
+
+  .list {
+    font-family: 'Lato', sans-serif;
+    background: linear-gradient(to right, rgba(76,173,200,1) 0%, rgba(81,172,195,1) 9%, rgba(99,169,175,1) 45%, rgba(128,163,144,1) 100%);
+  }
+</style>
+
+
